ad3b628eeaaa5f01579895aa35939d6a60fa7b37
keithi16 <141677778+keithizinn@users.noreply.github.com>
2024-09-18 00:27:57 -0300

    Building the Sidebar and Main Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Users from "./pages/Users"
 import Settings from "./pages/Settings"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
+import AppLayout from "./ui/AppLayout"
 
 function App() {
   return (
@@ -16,13 +17,16 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
-            <Route index element={<Navigate replace to='dashboard' />} /> 
-            <Route path='dashboard' element={<Dashboard /> } />
-            <Route path='dashboard' element={<Dashboard /> } />
-            <Route path='bookings' element={<Bookings /> } />
-            <Route path='cabins' element={<Cabins /> } />
-            <Route path='users' element={<Users /> } />
-            <Route path='account' element={<Settings /> } />
+            <Route element={<AppLayout />} >
+              <Route index element={<Navigate replace to='dashboard' />} /> 
+              <Route path='dashboard' element={<Dashboard /> } />
+              <Route path='dashboard' element={<Dashboard /> } />
+              <Route path='bookings' element={<Bookings /> } />
+              <Route path='cabins' element={<Cabins /> } />
+              <Route path='users' element={<Users /> } />
+              <Route path='account' element={<Settings /> } />
+            </Route>
+
             <Route path='login' element={<Login /> } />
             <Route path='*' element={<PageNotFound /> } />
           </Routes>
