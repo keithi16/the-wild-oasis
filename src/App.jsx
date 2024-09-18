4b6f06e1d3568ddf5578df730bed4def1447409f
keithi16 <141677778+keithizinn@users.noreply.github.com>
2024-09-18 00:01:10 -0300

    Setting up Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-function App() {
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
+import GlobalStyles from './styles/GlobalStyles'
+import Dashboard from "./pages/Dashboard"
+import Bookings from "./pages/Bookings"
+import Cabins from "./pages/Cabins"
+import Users from "./pages/Users"
+import Settings from "./pages/Settings"
+import Login from "./pages/Login"
+import PageNotFound from "./pages/PageNotFound"
+
+function App() {
   return (
-    <div>
-      Helllow World
-    </div>
+    <>
+      <GlobalStyles />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Navigate replace to='dashboard' />} /> 
+            <Route path='dashboard' element={<Dashboard /> } />
+            <Route path='dashboard' element={<Dashboard /> } />
+            <Route path='bookings' element={<Bookings /> } />
+            <Route path='cabins' element={<Cabins /> } />
+            <Route path='users' element={<Users /> } />
+            <Route path='account' element={<Settings /> } />
+            <Route path='login' element={<Login /> } />
+            <Route path='*' element={<PageNotFound /> } />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   )
 }
 
